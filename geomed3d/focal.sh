7b7db1795f63dfca2b7f4b4f210c71de5ea4d0a8
Alexey Pechnikov <pechnikov@mobigroup.ru>
2021-04-26 21:33:50 +0700

    Enhance focal calculation example notebook

--- geomed3d/focal.sh
+++ geomed3d/focal.sh
@@ -4,8 +4,8 @@ set -e
 CC="gcc-10 -O2 -mmmx -msse -lm -funroll-all-loops -fopenmp -fPIC -D_FILE_OFFSET_BITS=64 -Wall"
 CCC="$CC -o dynamically_linked -L. -lgeomed3dv4"
 
-rm -f test_mask
-$CCC test_mask.c -o test_mask
-file test_mask
+rm -f focal
+$CCC focal.c -o focal
+file focal
 
-./test_mask 1
+./focal 1
